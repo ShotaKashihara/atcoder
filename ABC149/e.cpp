17ac13704e4c141c8a367fd33f49b514b95042d1
Shota Kashihara <mercury4th@gmail.com>
2020-02-10 23:28:10 +0900

    ABC149 e RE

---
+++ ABC149/e.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define var auto
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  ll m;
+  cin >> n >> m;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  vector<int> aa(n*n);
+  rep(i, n) {
+    // cout << "i: " << i << endl;
+    rep(j, n) {
+      // cout << "j: " << i << endl;
+      aa[i*n + j] = a[i] + a[j];
+    }
+  }
+  sort(aa.rbegin(), aa.rend());
+  ll ans = 0;
+  rep(i, m) {
+    ans += aa[i];
+  }
+
+  cout << ans << endl;
+}
