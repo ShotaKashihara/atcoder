ffcdecb20ff9ad53b7617d3c52afda4b0785d7bd
Shota Kashihara <mercury4th@gmail.com>
2020-02-15 23:47:35 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,17 +4,39 @@
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
+#ifdef DEBUG
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; cout << a << endl; return 1; } cout << a << endl; return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; cout << a << endl; return 1; } cout << a << endl; return 0; }
+#else
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+#endif
 const ll INF = 1LL << 60;
-const int MAX_ITEM = 110;
-const int MAX_VALUE = 100100;
-ll dp[MAX_ITEM][MAX_VALUE] = {0};
+const int MAX_ITEM = 10e4+10; // 10^5
+ll dp[MAX_ITEM] = {0};
 
 int main() {
-  // 最小化問題なので INF で埋める
-  rep(i,100010) dp[i][0] = INF;
-  dp[0][0] = 0;
+  int n,k;
+  cin >> n >> k;
+  int h[n];
+  rep(i,n) cin >> h[i];
+
+  // 最小化問題なので INF or 0 で埋める
+  rep(i,MAX_ITEM) {
+    dp[i] = INF;
+  }
+   
+  // 初期値は 0 or INF
+  dp[0] = 0;
+
+  // 足場だけ回す
+  rep(i,n-1) {
+    // 足場i+j の値は 足場iのコストに abs(h[i] - h[i+j]) を足した数
+    rep(j,k+1) {
+      if (j == 0) continue;
+      chmin(dp[i+j], dp[i] + abs(h[i+j] - h[i]));
+    }
+  }
   
-  cout << dp[n-1][0] << endl;
+  cout << dp[n-1] << endl;
 }
