0ef15ad865bd8b5aae3eaec6d779f6708775e951
Shota Kashihara <mercury4th@gmail.com>
2020-02-22 02:08:58 +0900

    update

--- abc077/c.cpp
+++ abc077/c.cpp
@@ -4,19 +4,6 @@
 #define var auto
 using namespace std;
 using ll = long long;
-using P = pair<int, int>;
-// sort(A.begin(), A.end(), cmp)
-bool cmp(P a, P b) { return a.second < b.second; }
-#ifdef DEBUG
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; cout << a << endl; return 1; } cout << a << endl; return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; cout << a << endl; return 1; } cout << a << endl; return 0; }
-#else
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-#endif
-const ll INF = 1LL << 60;
-const int MAX_ITEM = 10e4+10; // 10^5
-ll dp[MAX_ITEM] = {0};
 
 int main() {
   int n;
