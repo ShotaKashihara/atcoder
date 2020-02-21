4e37ef951fc1585113cccdb12cf0d8b7d9917fbe
Shota Kashihara <mercury4th@gmail.com>
2020-02-22 02:06:28 +0900

    update readme

--- README.md
+++ README.md
@@ -23,14 +23,56 @@ rep(i,0,n) cin >> a[i];
 ```
 
 ```c++
+#define all(a) (a).begin(),(a).end()
+
 // asc sort
-sort(A.begin(), A.end());
+sort(all(A));
 
 // desc sort
 sort(A.rbegin(), A.rend());
 
 // compあり sort
-sort(a.begin(), a.end(), [](int a1, int a2) {
+sort(all(A), [](int a1, int a2) {
   return a1 > a2;
 });
+```
+## lower_bound, upper_bound
+
+```c++
+#include <bits/stdc++.h>
+using namespace std;
+#define all(a) (a).begin(),(a).end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
+vector<int> a = {1, 14, 32, 51, 51, 51, 243, 419, 750, 910};
+
+int main() {
+  int target = 51;
+
+  sort(all(a));
+  auto l_i = lower_bound(all(a), target);
+  auto u_i = upper_bound(all(a), target);
+
+  cout << "target: " << target << endl;
+  cout << "array.size(): " << a.size() << endl;
+  cout << "array: ";
+  rep(i,0,a.size()) cout << a[i] << ",";
+  cout << endl;
+  
+  // 51
+  cout << "(*lower_bound)target以上の最初の値: " << *l_i << endl;
+  // 243
+  cout << "(*upper_bound)targetより大きいの最初の値: " << *u_i << endl;
+
+  // 3
+  cout << "(lower_bound)target未満の個数:" << l_i - a.begin() << endl;
+  // 7
+  cout << "(lower_bound)target以上の個数:" << a.end() - l_i << endl;
+  // 4
+  cout << "(upper_bound)target以下の個数:" << a.end() - u_i << endl;
+  // 6
+  cout << "(upper_bound)targetより大きい個数:" << u_i - a.begin() << endl;
+
+  // 3
+  cout << "(upper - lower)targetと同値の個数:" << u_i - l_i << endl;
+}
 ```
--- playground.cpp
+++ playground.cpp
@@ -1,37 +1,29 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
+#define all(a) (a).begin(),(a).end()
+#define rep(i,a,b) for(int i=a;i<b;i++)
 vector<int> a = {1, 14, 32, 51, 51, 51, 243, 419, 750, 910};
 
-// index が条件を満たすかどうか
-bool isOK(int index, int key) {
-    if (a[index] >= key) return true;
-    else return false;
-}
-
-// 汎用的な二分探索のテンプレ
-int binary_search(int key) {
-
-  int ng = 20; // 初期値の左端
-  int ok = 36; // 初期値の右端
-
-  // 両端にある、leftとrightの差が 1 になるまでループする
-
-  while (abs(ok - ng) > 1) {
-    int mid = (ok + ng) / 2;
-
-    if (isOK(mid, key)) ok = mid; // isOK を満たしていたら、mid は ok
-    else ng = mid; // isOK を満たしていなければ、mid は ng
-  }
-}
-
 int main() {
-    cout << binary_search(51) << endl; // a[3] = 51 (さっきは 4 を返したが今回は「最小の index」なので 3)
-    cout << binary_search(1) << endl; // a[0] = 1
-    cout << binary_search(910) << endl; // a[9] = 910
+  int target = 51;
 
-    cout << binary_search(52) << endl; // 6
-    cout << binary_search(0) << endl; // 0
-    cout << binary_search(911) << endl; // 10 (場外)
+  sort(all(a));
+  auto l_i = lower_bound(all(a), target);
+  auto u_i = upper_bound(all(a), target);
+
+  cout << "target: " << target << endl;
+  cout << "array.size(): " << a.size() << endl;
+  cout << "array: ";
+  rep(i,0,a.size()) cout << a[i] << ",";
+  cout << endl;
+  
+  cout << "(*lower_bound)target以上の最初の値: " << *l_i << endl;
+  cout << "(*upper_bound)targetより大きいの最初の値: " << *u_i << endl;
+
+  cout << "(lower_bound)target未満の個数:" << l_i - a.begin() << endl;
+  cout << "(lower_bound)target以上の個数:" << a.end() - l_i << endl;
+  cout << "(upper_bound)target以下の個数:" << a.end() - u_i << endl;
+  cout << "(upper_bound)targetより大きい個数:" << u_i - a.begin() << endl;
+
+  cout << "(upper - lower)targetと同値の個数:" << u_i - l_i << endl;
 }
