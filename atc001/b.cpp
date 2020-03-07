93663ff0252ab6f1724e4bf6c679f04b25c0a34d
Shota Kashihara <mercury4th@gmail.com>
2020-03-07 15:44:40 +0900

    update

---
+++ atc001/b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i,a,n) for(int i=a;i<(a+n);i++)
+#define var auto
+using namespace std;
+using ll = long long;
+
+int main() {
+
+  int n, q;
+  vector<int> p, a, b;
+  rep(i, 0, q) {
+    
+  }
+
+  return 0;
+}
