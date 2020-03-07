1290d931f6f3ee633ef69ad73e2fa32d625b3734
Shota Kashihara <mercury4th@gmail.com>
2020-03-07 16:11:00 +0900

    atcoder_typical_contest_001_b

--- atc001/b.cpp
+++ atc001/b.cpp
@@ -4,12 +4,43 @@
 using namespace std;
 using ll = long long;
 
-int main() {
+struct UnionFind {
+  vector<int> par;
+  UnionFind(int N): par(N) {
+    rep(i, 0, N) par[i] = i;
+  }
+  int root(int x) {
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+  }
+  void unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if (rx == ry) return;
+    par[rx] = ry;
+  }
+  bool same(int x, int y) {
+    return root(x) == root(y);
+  }
+};
 
+int main() {
+  // input
   int n, q;
-  vector<int> p, a, b;
+  cin >> n >> q;
+  UnionFind uf(n);
+
+  // calculation
+  int p, a, b;
   rep(i, 0, q) {
-    
+    cin >> p >> a >> b;
+    if (p == 0) {
+      // 連結
+      uf.unite(a,b);
+    } else {
+      // 判定
+      cout << (uf.same(a,b) ? "Yes" : "No") << endl; 
+    }
   }
 
   return 0;
