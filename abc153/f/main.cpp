b68a6da41afa731aa2ee38297c7eaf441a18fb66
Shota Kashihara <mercury4th@gmail.com>
2020-03-21 21:53:01 +0900

    update

--- abc153/f/main.cpp
+++ abc153/f/main.cpp
@@ -6,40 +6,35 @@ using P = pair<int,int>;
 
 vector<int> a;
 int main() {
-  // int n,d,a;
-  // cin >> n >> d >> a;
-  // vector<P> xh(n);
-  // rep(i,n) {
-  //   int x,h; cin >> x >> h;
-  //   xh[i] = P(x, h);
-  // }
+  int n,d,a;
+  cin >> n >> d >> a;
+  vector<P> xh(n);
+  rep(i,n) {
+    int x,h; cin >> x >> h;
+    xh[i] = P(x, h);
+  }
 
-  // // 座標順にソートしてみる
-  // sort(xh.begin(), xh.end(), [](P p1, P p2) {
-  //   return p1.first < p2.first;
-  // });
+  // 座標順にソートしてみる
+  sort(xh.begin(), xh.end(), [](P p1, P p2) {
+    return p1.first < p2.first;
+  });
 
-  // ll ans = 0;
-  // // 左側から爆破
-  // rep(i, n) {
-  //   // 最初の 座標とHP
-  //   int index = xh[i].first;
-  //   int hp = xh[i].second;
-  //   // HP をゼロにするのに必要な爆弾の数
-  //   int bomb = (hp%a == 0) ? hp/a : hp/a +1;
-  //   ans += bomb;
-  //   // index から周辺 d にいるやつのHPを削る
-  //   rep(j,n) {
-  //     if (xh[j].first - index <= (d*2)) {
-  //       xh[j].second -= bomb*a;
-  //     }
-  //   }
-  // }
+  ll ans = 0;
+  // 左側から爆破
+  rep(i, n) {
+    // 最初の 座標とHP
+    int index = xh[i].first;
+    int hp = xh[i].second;
+    // HP をゼロにするのに必要な爆弾の数
+    int bomb = (hp%a == 0) ? hp/a : hp/a +1;
+    ans += bomb;
+    // index から周辺 d にいるやつのHPを削る
+    rep(j,n) {
+      if (xh[j].first - index <= (d*2)) {
+        xh[j].second -= bomb*a;
+      }
+    }
+  }
 
-  int n; cin >> n;
-  a.resize(n);
-  rep(i,n) cin >> a[i];
-
-  cout << a[3] << endl;
   return 0;
 }
