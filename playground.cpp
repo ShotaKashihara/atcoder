8becfa32d41ebfcbaf54435584d3009896113047
Shota Kashihara <mercury4th@gmail.com>
2020-03-22 23:57:45 +0900

    update

--- playground.cpp
+++ playground.cpp
@@ -8,12 +8,20 @@ int main() {
   int a = 50000;
   long long  l = 50000;
 
-  long long ans;
+  // long long ans;
 
-  ans = a*a;
-  cout << ans << endl; // -1794967296 ← overflowしてる
-  ans = (ll)a*a;
-  cout << ans << endl; // -1794967296 ← overflowしてる
-  ans = l*l;
+  long long ans = 50000 * 50000;
+  cout << ans << endl; // overflow
+
+  long long ans = (long long)50000 * 50000;
   cout << ans << endl; // 2500000000
+
+  // ans = a*a;
+  // cout << ans << endl; // -1794967296 ← overflowしてる
+  // ans = (ll)a*a;
+  // cout << ans << endl; // -1794967296 ← overflowしてる
+  // ans = l*l;
+  // cout << ans << endl; // 2500000000
+
+
 }
